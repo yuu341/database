5aa90ecd374925858fdb85b09fe7a6edeb530458
Yusuke Furuya <oz.g.341@gmail.com>
2019-06-06 23:42:12 +0900

    MainWindowViewModelをとりあえずそろえる

---
+++ TableAnalyzer/TableAnalyzer/Models/Column.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableAnalyzer.Models
+{
+    class Column
+    {
+        /// <summary>
+        /// 英語名
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// 日本語名
+        /// </summary>
+        public string JapaneseName { get; private set; }
+
+        /// <summary>
+        /// 外部参照
+        /// </summary>
+        public Table Relation { get; private set; }
+    }
+}
--- TableAnalyzer/TableAnalyzer/ViewModels/MainWindowViewModel.cs
+++ TableAnalyzer/TableAnalyzer/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,49 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TableAnalyzer.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
         public MainWindowViewModel()
         {
-
+            Analyze.Subscribe(analyze);
+            InputFile.Value = @"C:\clud\input.xlsx";
+            OutputFile.Value = @"C:\clud\output.xlsx";
         }
+
+        private void analyze()
+        {
+        }
+
+        /// <summary>
+        /// Excelを指定
+        /// </summary>
+        public ReactiveProperty<string> InputFile { get; } = new ReactiveProperty<string>();
+
+        /// <summary>
+        /// 出力場所
+        /// </summary>
+        public ReactiveProperty<string> OutputFile { get; } = new ReactiveProperty<string>();
+
+        public ReactiveCommand Analyze { get; } = new ReactiveCommand();
+
+
+        #region
+
+        /// <summary>
+        /// 基本的にReactivePropertyだけで行けるのだが、C#の使用上メモリリーク対策として必要
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string parameter)
+         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(parameter));
+
+
+        #endregion
     }
 }
