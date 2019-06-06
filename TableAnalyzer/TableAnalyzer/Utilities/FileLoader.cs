875266a2adde7498dcfe62d7d0048852bb1e26a9
Yusuke Furuya <oz.g.341@gmail.com>
2019-06-07 00:08:33 +0900

    読み取り専用でロード

---
+++ TableAnalyzer/TableAnalyzer/Utilities/FileLoader.cs
@@ -0,0 +1,26 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableAnalyzer.Utilities
+{
+    class FileLoader
+    {
+        public FileLoader()
+        {
+
+        }
+
+        public void Load(string path)
+        {
+            FileStream inputStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            IWorkbook book = WorkbookFactory.Create(inputStream);
+
+            var sheet = book.GetSheet("list");
+        }
+    }
+}
